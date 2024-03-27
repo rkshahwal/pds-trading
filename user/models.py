9e6c15c7387d1ddd73f053cbaa4132cd44c37b56
rajendraswooshelf <rajendras@wooshelf.com>
2024-03-27 23:29:23 +0530

    changes

--- user/models.py
+++ user/models.py
@@ -92,7 +92,7 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return str(self.name)+" - "+str(self.mobile_number)
+        return str(self.email)+" - "+str(self.mobile_number)
     
     def save(self, *args, **kwargs):
         """ This method is used to modify the password field
